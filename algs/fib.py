9949cb4d38f535a66ee18b15c7bbcbaad9fe56ec
WILLIAM XIAO <wxiao@college.harvard.edu>
2013-04-13 00:40:59 -0400

    fib.py added

---
+++ algs/fib.py
@@ -0,0 +1,23 @@
+"""
+CS51 Final Project
+Eamon, David, Kevin, Willy
+
+fib.py by Willy Xiao
+willy@chenxiao.us
+
+fib.py is a module that implements the fibonacci coding compression: 
+[citation]
+
+1. fib.compress compresses a file outputting FileName.fib to disk. 
+2. fib.decompress takes in a file outputting FileName.FileType to disk. 
+"""
+
+# compress takes in a string of the file name to compress 
+# and outputs a compressed file to disk
+def compress(file):
+  print file;
+
+# decompress takes in a string of the file name to decompress 
+# and outputs a decompressed file to disk 
+def decompress(file):
+  print file;
