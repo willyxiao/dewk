df0f4bb38f90e7db2164052bdcacf738695a699b
WILLIAM XIAO <wxiao@college.harvard.edu>
2013-04-20 23:18:53 -0400

    working

--- algs/fib.py
+++ algs/fib.py
@@ -13,28 +13,66 @@ fib.py is a module that implements the fibonacci coding compression:
 """
 import unittest 
 import helpers
+import io
 
 READ_IN_SIZE = 1 
 ALG_NAME = "fib"
+BYTE_SIZE = 8
 
+global buffer
+buffer = 0x00
+
+global buffer_pos
+buffer_pos = 0 
+    
 ###COMPRESS###
 
 # compress takes in a string of the file name to compress 
 # and outputs a compressed file to disk
 def compress(file_in_name):
+    global buffer
+    global buffer_pos
 
-  file_in = open(file_in_name, "r")
-  
-  i = file_in.read(READ_IN_SIZE)
-  
-  while (i != ''):
-                
-  
+    # opens output file and signs it 
+    (file_out_name, sign) = helpers.ensign(file_in_name, ALG_NAME)  
+    file_out = io.open(file_out_name, "wb") 
+    file_out.write(sign)
+        
+    file_in = io.open(file_in_name, "r")
+    i = file_in.read(READ_IN_SIZE)
+    
+    while (i != ''):
+    
+        enc = _encode_int(ord(i))
+        leng = len(enc) 
+        
+        if (leng + buffer_pos) < BYTE_SIZE :            
+            j = 0
+            while (j < leng) : 
+                buffer = buffer | (int(enc[j]) << (leng - (j + 1)))
+                j += 1
+            
+        elif (leng + buffer_pos) == BYTE_SIZE : 
+            j = 0 
+            while (j < leng) : 
+                buffer = buffer | (int(enc[j]) << (leng - (j + 1)))
+                j += 1
+            file_out.write(buffer)
+            buffer = 0x00
+        
+        else : #leng + buffer_pos > BYTE_SIZE : 
+            j = 0 
+            while (j < (BYTE_SIZE - buffer_pos)) : 
+                buffer = buffer | (int(enc[j]) << ((BYTE_SIZE - buffer_pos) - (j + 1)))
+                j += 1
+            file_out.write(buffer)
+            buffer = 0x00
+    
+        buffer_pos += (leng % BYTE_SIZE)     
+        i = file_in.read(READ_IN_SIZE)
 
-  file_in.close(); 
-
-
-# returns the beginning of the file_type
+    file_in.close(); 
+    file_out.close();
 
 
 # encodes a single integer less than or equal to 1 into a fibonacci sequence. 
