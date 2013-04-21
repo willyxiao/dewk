3968e204ff6817d92d28ec73198338419ca18a49
Kevin Kirkor Eskici <keskici@college.harvard.edu>
2013-04-21 10:28:52 -0400

    Sun 10:28

--- algs/huff.py
+++ algs/huff.py
@@ -4,4 +4,48 @@ Eamon, David, Kevin, Willy
 
 huff.py by Kevin Eskici
 keskici@college.harvard.edu
+'''
+
+import io
+import helpers
+import queue
+# installed bitstream for this: 
+# "sudo python setup.py install" after downloading from
+# http://code.google.com/p/python-bitstring/
+from bitstream import BitStream, BitArray
+
+READ_IN_SIZE = 1
+ALG_NAME = "huf"
+BYTE_SIZE = 8
+
+global buffer
+buffer = 0x00
+
+global buffer_pos
+buffer_pos = 0
+
+### COMPRESS ###
+
+def compress(file_in_name):
+    global buffer
+    global buffer_pos
+
+    #open output file and sign
+    (file_out_name, sign) = helpers.ensign(file_in_name, ALG_NAME)
+    file_out = io.open(file_out_name, "wb")
+    file_out.write(sign)
+
+    file_in = io.open(file_in_name, "r")
+    i = file_in.read(READ_IN_SIZE)
+
+
+
+#insert actual compression part
+
+
+    file_in.close();
+    file_out.close();
+
+def _build_tree(
+
 
