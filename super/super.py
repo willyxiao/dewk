cefdc95dc11dd5600bf68e2717e8abe5e497df82
WILLIAM XIAO <wxiao@college.harvard.edu>
2013-05-02 00:22:56 -0400

    pushing

--- super/super.py
+++ super/super.py
@@ -11,11 +11,45 @@ uses that algorithm to encode the file
 super.decompress decompresses a file
 """
 import study
+import s_helpers
+
+import subprocess
+import sys
+import os
 
 def compress(file_name) :
+
+    # ensure file_name is actually the name of a file
+    s_helpers.check_file(file_name) 
+        
+    # best_algorithm based upon each alg's estimation
     alg = study.best_alg(file_name)
-    return alg.compress(file_name)
+    
+    # if the compression fails, then raise an error
+    try : 
+        compressed_file = alg.compress(file_name)
+    except : 
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
+    # else remove the original file and return the name of the compressed file
+    else : 
+        subprocess.call(["rm", "-f", file_name])
+        return compressed_file
 
 def decompress(file_name) : 
+    
+    s_helpers.check_file(file_name)
+    
+    # return the algorithm used to compress the file
     alg = study.get_alg(file_name)
-    return alg.decompress(file_name)
+    
+    ## same as compress except with decompress
+    try : 
+        decompressed_file = alg.decompress(file_name)
+    except : 
+        print "Unexpected error:", sys.exec_info()[0]
+        raise
+    else : 
+        subprocess.call(["rm", "-f", file_name])
+        return decompressed_file
+        
