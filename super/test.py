661a51df4455e7d5c962902c954d69eab7d2d610
WILLIAM XIAO <wxiao@college.harvard.edu>
2013-05-05 00:07:56 -0400

    testing is done

--- super/test.py
+++ super/test.py
@@ -9,6 +9,7 @@ import filecmp
 import unittest 
 import os
 import shutil
+import time
 import sys 
 sys.path.append("../")
 
@@ -16,6 +17,7 @@ from algs import *
 import algs
 import super
 import dir
+import s_helpers
 
 # the algorithms listed
 algos = {
@@ -26,12 +28,6 @@ algos = {
     "lzw" : algs.lzw, 
     #"seq" : algs.seq
 }
-
-# to test everything from terminal
-if len(sys.argv) > 2 : 
-    print "Usage: python test.py test_file_name test_directory_name" 
-else : 
-    test_all(sys.argv[1], sys.argv[2])
 
 # tests all the individual algorithms
 def test_algs (file_name) : 
@@ -59,9 +55,31 @@ def test_dir (dir_name) :
     dir.decompress(dir.compress(dir_name))
 
 # test all of 'em
-def test_all(test_file, test_dir) : 
-    test_algs(test_file)
-    test_super(test_dir) 
-    test_dir(test_dir) 
+def test_all(test_file_name, test_dir_name) : 
+    
+    print_with_sleep("Testing algs...") 
+    test_algs(test_file_name)
+    print_with_sleep("All algs passed!") 
+    
+    print_with_sleep("Testing super...")
+    test_super(test_dir_name)
+    print_with_sleep("Super passed!")
+    
+    print_with_sleep("Testing dir...") 
+    test_dir(test_dir_name)
+    print_with_sleep("Dir passed!")
+    
+    print "All tests passed! Yay!"  
 
-
+# prints a message with a delay
+def print_with_sleep(message) : 
+    s_helpers.print_single_line()
+    print message
+    s_helpers.print_single_line()
+    time.sleep(2)
+    
+# to test everything from terminal
+if len(sys.argv) != 3 : 
+    print "Usage: python test.py test_file_name test_directory_name" 
+else : 
+    test_all(sys.argv[1], sys.argv[2])
